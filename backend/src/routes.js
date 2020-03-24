b05d3ff3a3289619a10bda9c89632f359da5c8dd
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-24 17:34:40 -0300

    creating the profile controller and route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const ngoController = require('./controllers/ngosController')
+const profileController = require('./controllers/profileController')
 const incidentController = require('./controllers/incidentesController')
 
 // Query params: used to filter something.
@@ -10,6 +11,9 @@ const incidentController = require('./controllers/incidentesController')
 // NGO's routes.
 routes.get('/ngos', ngoController.index)
 routes.post('/ngos', ngoController.create)
+
+// NGO profile.
+routes.get('/profile', profileController.index)
 
 // Incidents routes.
 routes.get('/incidents', incidentController.index)
