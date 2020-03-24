260075f3de1744a1e7d1a9f122b749d59662995d
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-24 17:40:12 -0300

    creating an error message to the profile route

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -7,6 +7,10 @@ module.exports = {
         const incidents = await connection('incidents')
         .where('ngo_id', ngo_id).select('*')
 
+        if(Object.keys(incidents).length === 0) {
+            return res.json({error: "Either this NGO has no incidents or it doesn't exist"})
+        }
+
         return res.json(incidents)
     }
 }
