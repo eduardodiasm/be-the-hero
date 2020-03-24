b61262582f8ad61972763585842f2e8dc73e2278
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-24 14:08:23 -0300

    crating the ngos table

--- backend/src/database/migrations/20200324140119_create_ngos.js
+++ backend/src/database/migrations/20200324140119_create_ngos.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  knex.schema.createTable('ngos', function(table) {
+      table.string('id').primary()
+      table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('whatsapp').notNullable()
+      table.string('city').notNullable()
+      table.string('state', 2).notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('ngos')
 };
