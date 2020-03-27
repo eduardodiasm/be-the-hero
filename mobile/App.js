631f5a934eb50581e8564b96e2cc65cdc7360697
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-27 17:05:31 -0300

    restarting the mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
