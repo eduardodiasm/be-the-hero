6c9575a9d732e84e530a833b2ad691fd79f84cd7
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-28 00:52:23 -0300

    creating the test for generate unique id function

--- backend/src/controllers/ngosController.js
+++ backend/src/controllers/ngosController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto')
+const generateUniqueId = require('../utils/generateUniqueId')
 const connection = require('../database/connection')
 
 module.exports = {
@@ -10,8 +10,8 @@ module.exports = {
 
     async create(req, res) {
         const { name, email, whatsapp, city, state } = req.body
-        const id = crypto.randomBytes(4).toString('HEX')
-
+        const id = generateUniqueId()
+        
         await connection('ngos').insert({
             id,
             name,
